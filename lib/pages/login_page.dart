7d3dad2b5a807315ea26acee2f989a0d6844bf2e
Emmanuel Chargoy <charger513@hotmail.com>
2021-07-21 14:39:13 -0500

    Diseño del LoginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,9 +6,100 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      backgroundColor: Color(0xFFF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text(
+              'Terminos y condiciones de uso',
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 50),
+        width: 170,
+        child: Column(
+          children: [
+            Image.asset('assets/tag-logo.png'),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Messenger',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          TextField(),
+          TextField(),
+          ElevatedButton(onPressed: () {}, child: null)
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "¿No tienes cuenta?",
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        GestureDetector(
+          child: Text(
+            '¡Crea una ahora!',
+            style: TextStyle(
+              color: Colors.blue[600],
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
