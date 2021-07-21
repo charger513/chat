a2a3ff6af57908b3d19aad9f68409977bcae04a1
Emmanuel Chargoy <charger513@hotmail.com>
2021-07-21 18:28:37 -0500

    ChatPage - Inicio

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,8 +4,52 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat Page'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 1,
+        title: Column(
+          children: [
+            CircleAvatar(
+              maxRadius: 14,
+              child: Text(
+                'Te',
+                style: TextStyle(fontSize: 12),
+              ),
+              backgroundColor: Colors.blue[100],
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Text(
+              'Emmanuel Chargoy',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 100,
+                reverse: true,
+                itemBuilder: (ctx, index) {
+                  return Text('$index');
+                },
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
