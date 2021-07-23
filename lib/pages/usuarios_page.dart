3da7358def025972cc163e16899e248d14c80b64
Emmanuel Chargoy <charger513@hotmail.com>
2021-07-22 19:20:45 -0500

    Logout de nuestra aplicación

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,4 +1,6 @@
+import 'package:chat/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import '../models/usuario.dart';
@@ -20,10 +22,11 @@ class _UsuariosPageState extends State<UsuariosPage> {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            'Mi Nombre',
+            authService.usuario!.nombre,
             style: TextStyle(
               color: Colors.black87,
             ),
@@ -35,7 +38,11 @@ class _UsuariosPageState extends State<UsuariosPage> {
               Icons.exit_to_app,
               color: Colors.black87,
             ),
-            onPressed: () {},
+            onPressed: () {
+              // TODO: desconectar soket server
+              AuthService.deleteToken();
+              Navigator.pushReplacementNamed(context, 'login');
+            },
           ),
           actions: [
             Container(
